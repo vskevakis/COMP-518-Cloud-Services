fb2db23e6d57d0f6b1dbfb886c4a8b07f88954a6
Vasilis Skevakis <vskevakis@gmail.com>
2020-12-23 10:57:17 +0200

    final tweaks

--- project_2/front-newui/src/pages/Admin.js
+++ project_2/front-newui/src/pages/Admin.js
@@ -6,7 +6,11 @@ class Admin extends Component {
 
     render() {
         return (
-            <h2> Please visit keyrock on port 3001 to access the admin panel </h2>
+            <div class="relative dark:bg-dark-dark bg-white min-h-full">
+                <div class="max-w-7xl mx-auto">
+                    <h2 class="center text-white dark:text-white"> Please visit keyrock on port 3001 to access the admin panel </h2>
+                </div>
+            </div>
         );
     }
 }
